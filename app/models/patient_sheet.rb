cee461c767ae6700c48d3e63e77954ef0792d867
Ghita Aachab <ghita.aachab@gmail.com>
2024-01-31 16:18:44 +0100

    fix validatios

--- app/models/patient_sheet.rb
+++ app/models/patient_sheet.rb
@@ -3,7 +3,7 @@ class PatientSheet < ApplicationRecord
   has_many :diseases
   has_many :allergies
 
-  validates :height, numericality: true, message: "Must be a numerical value."
-  validates :weight, numericality: true, message: "Please enter a valid numerical weight."
+  validates :height, numericality: { message: "Must be a numerical value."}
+  validates :weight, numericality:{ message: "Please enter a valid numerical weight."}
   validates :blood_type, presence: true, inclusion: { in: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"] }
 end
