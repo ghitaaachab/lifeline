1deb90605e956d21d9993f158a30eafab856bac5
Arbia el abboubi <sara01.abid@gmail.com>
2024-02-01 17:53:39 +0100

    vital contoller

--- app/controllers/vital_signs_controller.rb
+++ app/controllers/vital_signs_controller.rb
@@ -1,11 +1,28 @@
 class VitalSignsController < ApplicationController
-
   def index
-    
-  end
-  def create
-  end
- def new
- end
 
+    @vital_signs = VitalSigns.all
+  end
+
+  def new
+    @vital_sign = VitalSign.new
+  end
+
+  def create
+    @vital_sign = VitalSign.new(vital_sign_params)
+    @vital_sign.user_id = current_user.id
+
+
+    if @vital_sign.save
+      redirect_to dashboards_path, notice: 'Vital sign added successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def vital_sign_params
+    params.require(:vital_sign).permit(:label, :value)
+  end
 end
