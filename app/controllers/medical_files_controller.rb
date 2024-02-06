c4e1756ffc2f0e657f9f5db138988f3f54a0b941
Amine Bagui <med.bagui.1@gmail.com>
2024-02-06 22:23:02 +0100

    medical files are now downloadable & can be previewed as pdf

--- app/controllers/medical_files_controller.rb
+++ app/controllers/medical_files_controller.rb
@@ -46,22 +46,60 @@ class MedicalFilesController < ApplicationController
   end
 
   def preview
-    @file = MedicalFile.find_by(file_id: params[:id])
-    birth_date = @file.user.date_of_birth
-    age_str = calculate_age(birth_date)
+    pdf_report(:inline)
+  end
 
-    report = Prawn::Document.new
-    report.text "#{@file.name}", size: 30, style: :bold, align: :center, leading: 20
-    report.text "#{@file.user.first_name} #{@file.user.last_name}"
-    report.text "#{@file.user.address}"
-    report.text "#{age_str}"
-    report.text @file.description
-    report.start_new_page
-    report.text 'This Page for Photos'
-    send_data(report.render,  filename: 'datareport.pdf', type: "application/pdf", disposition: "inline")
+  def download
+    pdf_report(:attachment)
   end
 
   private
+
+  def pdf_report(disposition)
+    @file = MedicalFile.find_by(file_id: params[:id])
+    birth_date = @file.user.date_of_birth
+    age_str = calculate_age(birth_date)
+    test_images = []
+    prescription_images = []
+
+    report = Prawn::Document.new
+    report.text "", leading: 10
+    report.text "#{@file.name}", size: 30, style: :bold, align: :center, leading: 40
+    report.text "#{@file.user.first_name} #{@file.user.last_name}"
+    report.text "#{@file.user.address}"
+    report.text "#{age_str}"
+    report.text @file.created_at.strftime("%B %d, %Y"), leading: 40
+    report.text "Treating Doctor: #{@file.treating_dr}", leading: 30, style: :bold
+    report.text @file.description, leading: 20
+    report.text @file.notes
+
+
+    @file.tests.each do |test|
+      test.photos.each do |photo|
+        test_images << StringIO.open(photo.download)
+      end
+    end
+
+    @file.prescriptions.each do |prescription|
+      prescription.photos.each do |photo|
+        prescription_images << StringIO.open(photo.download)
+      end
+    end
+    report.start_new_page
+    report.text "Medical Prescriptions", size: 24, style: :bold, align: :center, leading: 40
+
+    prescription_images.each do |image|
+      report.image(image, width: 500, position: :center)
+    end
+    report.start_new_page
+    report.text "Medical Tests:", size: 24, style: :bold, align: :center, leading: 40
+
+    test_images.each do |image|
+      report.image(image, width: 500, position: :center)
+    end
+
+    send_data(report.render,  filename: 'datareport.pdf', type: "application/pdf", disposition: disposition)
+  end
 
   def calculate_age(birth_date)
     today = Date.today
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/dashboard", to: "dashboards#index", as: :dashboard
   get "/medical_files/:id/download", to: "medical_files#download", as: :download_medical_file
-  get "/medical_files/:id/preview", to: "medical_files#preview", as: :preview_medical_file
+  get "/medical_files/:id/preview_file", to: "medical_files#preview", as: :preview_medical_file
   resources :appointments, only: [:index, :create, :destroy, :show]
   resources :medical_files, only: [:index, :create, :destroy, :show]
   resources :vital_signs, only: [:index, :new, :create]
