4d1389e4c146bc29e966b07bf2ed671df8ad4524
Ghita Aachab <ghita.aachab@gmail.com>
2024-02-08 13:50:55 +0100

    fixed design

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @patient_sheet = PatientSheet.find_by(user_id: current_user.id)
     @vital_signs = VitalSign.all
     @medical_files = MedicalFile.where(user: current_user)
   end
